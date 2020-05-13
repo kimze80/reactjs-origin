b79e4b5189d44bacf2d85d44ac61e5114282196b
kimze <kimze80@gmail.com>
2020-05-13 14:34:31 -0300

    packages update

--- dist/src.f69400ca.js
+++ dist/src.f69400ca.js
@@ -55536,7 +55536,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53680" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55059" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
